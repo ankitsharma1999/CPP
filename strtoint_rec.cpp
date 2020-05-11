2244d9fd56a5de8ef9275e571a1d27ca27f1dab3
Ankit <ank.shrma.1999@gmail.com>
2020-05-11 13:13:28 +0530

    Added

---
+++ strtoint_rec.cpp
@@ -0,0 +1,27 @@
+# include<iostream>
+# include<cmath>
+using namespace std;
+
+void toInt(string str, int& n, int index)
+{
+    if(index == str.length())
+    {
+        return;
+    }
+    else
+    {
+        toInt(str, n, index+1);
+        int digit = (int)str[index] - 48;
+        n += digit*(pow(10, str.length()-index-1));
+    }
+}
+
+
+int main()
+{
+    string str="1234";
+    int n=0;
+    toInt(str, n, 0);
+    cout<<n<<endl;
+    return 0;
+}
