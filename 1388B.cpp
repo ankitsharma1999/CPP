4ec15a3b9ab4a33d32b6986202ab2a8cea49338e
Ankit <ank.shrma.1999@gmail.com>
2020-08-14 19:05:41 +0530

    Add new Codeforces Problem

---
+++ 1388B.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+string solve(int);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << solve(n) << "\n";
+    }
+    return 0;
+}
+
+string solve(int n)
+{
+    string res = "";
+    int num = 0;
+    num = n / 4;
+    if (n % 4)
+        num++;
+    for (int i = n; i >= 1; i--)
+    {
+        if (i > num)
+        {
+            res += "9";
+        }
+        else
+        {
+            res += "8";
+        }
+    }
+    return res;
+}
